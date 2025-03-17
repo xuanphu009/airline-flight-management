92333d56b348ecdd6e64f3962f12e6c6dee8dd73
Dka PTIT <ekko.okke666@gmail.com>
2025-03-17 14:52:21 +0700

    update enter

--- include/Constants.h
+++ include/Constants.h
@@ -6,6 +6,30 @@ extern const int LEN_CMND = 15;
 
 
 
-// void enter(char *str, int len_str, std::vector<function<bool(char)>> conditions) {
+template <typename... Conds>
+void enter(char *str, int &index, int max_len, char &ch, auto... conditions) {
+    while (true) {
+        ch = _getch();
 
-// }
+        // Loại bỏ mã tiền tố của phím UP, DOWN
+        if (ch == -32 || ch == 224) {
+            ch = _getch(); // Bỏ qua mã tiền tố, lấy mã thực
+            return; // Thoát nhập ngay khi gặp UP/DOWN
+        }
+
+        if (ch == ENTER) return;
+
+        if (ch == BACKSPACE) { // Nhấn Backspace
+            if (index > 0) {
+                cout << "\b \b"; // Xóa ký tự trên màn hình
+                str[--index] = '\0';
+            }
+        } else if (index < max_len - 1) { // Chưa vượt quá giới hạn
+            if ((... && conditions(ch))) { // Fold expression kiểm tra nhiều điều kiện
+                cout << ch;
+                str[index++] = ch;
+                str[index] = '\0';
+            }
+        }
+    }
+}
