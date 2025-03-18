95bae4afb44cca8ab78f49c103b5ebaf951d6554
Dka PTIT <ekko.okke666@gmail.com>
2025-03-19 00:04:32 +0700

    update FE

--- include/Console.h
+++ include/Console.h
@@ -10,79 +10,6 @@ namespace Console {
     AVL_TREE manager;
     Flight *list;
     void book_ticket(Flight *booking) {
-        
-        Passenger input;
-        int idx[4] = {}; // Mảng lưu vị trí nhập từng trường
-        int column = 0;
-        char ch;
-        while (true) {
-            // display(input, column); // Hiển thị lại dữ liệu trước khi nhập
-    
-            // Nhập dữ liệu tương ứng với dòng hiện tại
-            switch (column) {
-                case 0:
-                    enter(input.CMND, idx[column], LEN_CMND, ch);
-                    break;
-                case 1:
-                    enter(input.last_name, idx[column], LEN_LAST_NAME, ch, 
-                        [&](char &c) {
-                            if(c >= 'a' && c <= 'z') c -= 32;
-                            return c >= 'A' && c <= 'Z';
-                        }
-                    );
-                    break;
-                case 2:
-                    enter(input.first_name, idx[column], LEN_FIRST_NAME, ch, 
-                        [&](char &c) {
-                            if(c >= 'a' && c <= 'z') c -= 32;
-                            return c >= 'A' && c <= 'Z';
-                        }
-                    );
-                    break;
-                case 3: {
-                    char tmp = '\0';
-                    enter(&tmp, idx[column], 2, ch, 
-                        [&](char &c) {
-                            return c <= '1' && c >= '0';
-                        }
-                    ); // 2 để có '\0'
-                    if (tmp) input.gender = new bool(tmp - '0');
-                    break;
-                }
-            }
-    
-            // Nếu nhấn UP hoặc DOWN, xử lý luôn ngoài vòng lặp chính
-            if (ch == UP && column > 0) {
-                --column;
-            } else if (ch == DOWN && column < 3) {
-                ++column;
-            } else if (ch == ENTER && input.valid_user()) {
-
-                continue; // Xử lý sau nếu cần
-            } //nhấn esc để out
-        }
-
-        //input.CMND có thể rỗng
-        //input.CMND có thể trùng và khác last name, fist name trong cây 
-
-        if(!booking->valid_CMND(input.CMND)) return;
-
-        int new_seat; 
-        while(std::cin >> new_seat) {
-            if(booking->valid_seat(new_seat)) break;
-        }
-
-        Node *user = manager.search(manager.get_root() ,input.CMND);
-        if(user == nullptr) {
-            manager.insert(manager.get_root(), input);
-        } else {
-            if(!(user->data == input)) {
-                //lỗi
-            } 
-        }
-
-        Ticket new_ticket(new_seat, input.CMND);
-        booking->tickets[new_seat] = new_ticket; 
  
 
 
@@ -111,6 +38,82 @@ namespace Console {
 
     //     strcpy(flight_code, cur.used)
 
+    // }
+
+
+    // void buy_ticket() {
+    //     Passenger input;
+    //     int idx[4] = {}; // Mảng lưu vị trí nhập từng trường
+    //     int column = 0;
+    //     char ch;
+    //     while (true) {
+    //         // display(input, column); // Hiển thị lại dữ liệu trước khi nhập
+    
+    //         // Nhập dữ liệu tương ứng với dòng hiện tại
+    //         switch (column) {
+    //             case 0:
+    //                 enter(input.CMND, idx[column], LEN_CMND, ch);
+    //                 break;
+    //             case 1:
+    //                 enter(input.last_name, idx[column], LEN_LAST_NAME, ch, 
+    //                     [&](char &c) {
+    //                         if(c >= 'a' && c <= 'z') c -= 32;
+    //                         return c >= 'A' && c <= 'Z';
+    //                     }
+    //                 );
+    //                 break;
+    //             case 2:
+    //                 enter(input.first_name, idx[column], LEN_FIRST_NAME, ch, 
+    //                     [&](char &c) {
+    //                         if(c >= 'a' && c <= 'z') c -= 32;
+    //                         return c >= 'A' && c <= 'Z';
+    //                     }
+    //                 );
+    //                 break;
+    //             case 3: {
+    //                 char tmp = '\0';
+    //                 enter(&tmp, idx[column], 2, ch, 
+    //                     [&](char &c) {
+    //                         return c <= '1' && c >= '0';
+    //                     }
+    //                 ); // 2 để có '\0'
+    //                 if (tmp) input.gender = new bool(tmp - '0');
+    //                 break;
+    //             }
+    //         }
+    
+    //         // Nếu nhấn UP hoặc DOWN, xử lý luôn ngoài vòng lặp chính
+    //         if (ch == UP && column > 0) {
+    //             --column;
+    //         } else if (ch == DOWN && column < 3) {
+    //             ++column;
+    //         } else if (ch == ENTER && input.valid_user()) {
+
+    //             continue; // Xử lý sau nếu cần
+    //         } //nhấn esc để out
+    //     }
+
+    //     //in ra danh sách chuyến bay
+    //     // nhập p
+
+    //     if(!booking->valid_CMND(input.CMND)) return;
+
+    //     int new_seat; 
+    //     while(std::cin >> new_seat) {
+    //         if(booking->valid_seat(new_seat)) break;
+    //     }
+
+    //     Node *user = manager.search(manager.get_root() ,input.CMND);
+    //     if(user == nullptr) {
+    //         manager.insert(manager.get_root(), input);
+    //     } else {
+    //         if(!(user->data == input)) {
+    //             //lỗi
+    //         } 
+    //     }
+
+    //     Ticket new_ticket(new_seat, input.CMND);
+    //     booking->tickets[new_seat] = new_ticket; 
     // }
 
     // void 
---
+++ include/Menu.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+
+struct Menu {
+    void gotoxy(int x, int y);
+    void show_navigation_instructions();
+    void print_login_frame();
+    void print_manager_menu();
+    void manage_aircrafts();
+    void add_aircraft();
+    void delete_aircraft();
+    void edit_aircraft_details();
+    void manage_flights();
+    void create_new_flight();
+    void edit_flight_schedule();
+    void cancel_flight();
+    void enter_user_information();
+    void identification_information();
+    void flight_list();
+    void book_ticket();
+    void cancel_ticket();
+    void passenger_list();
+    void available_flights();
+    void available_tickets();
+    void display_aircraft_statistics();
+
+    void print_main_menu();
+    void print_manage_planes();
+    void print_manage_flights();
+    void print_ticket_booking();
+    void print_cancel_ticket();
+    void print_passenger_list();
+    void print_available_flights();
+   //cac ham ben duoi can viet them
+    // void display_flight_list();
+    // void display_booked_flights();
+    // void display_passenger_list();
+    // void display_available_flights();
+    // void display_available_tickets();
+    // void function();
+};
+
+
