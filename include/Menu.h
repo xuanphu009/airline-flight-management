6b250840d53068488660ab8cfed642a9a389f492
danielnguyen611 <thaiizdabest@gmail.com>
2025-04-05 22:37:13 +0700

    menu

--- include/Menu.h
+++ include/Menu.h
@@ -7,34 +7,43 @@
 class Menu {
 public:
     void gotoxy(int x, int y);
+    void show_flight_manager_menu2(); 
     void show_navigation_instructions();
+    void show_list_instructions();
     void print_login_frame();
-    void print_manager_menu();
+    void show_flight_manager_menu();
+
+    // Aircraft Management
     void manage_aircrafts();
     void add_aircraft();
     void delete_aircraft();
     void edit_aircraft_details();
+    void show_plane_list();
+
+        // Flight Management
     void manage_flights();
+    void search_flight();
     void create_new_flight();
     void edit_flight_schedule();
-    void cancel_flight();
-    void enter_user_information();
-    void identification_information();
-    void flight_list();
+    void enter_flight_ID();
+
+    // Ticket Management
     void book_ticket();
     void cancel_ticket();
+
+    // Passenger Information
     void passenger_list();
+
+    // Flight Queries
     void available_flights();
     void available_tickets();
     void display_aircraft_statistics();
-    
-   //cac ham ben duoi can viet them
-    void display_flight_list();
-    void display_booked_flights();
-    void display_passenger_list();
-    void display_available_flights();
-    void display_available_tickets();
-    void function();
+    void show_flight_list();
+
+        // User Information
+    void enter_user_information();
+    void identification_information();
+    void enter_flight_details();
 };
 
 #endif 
